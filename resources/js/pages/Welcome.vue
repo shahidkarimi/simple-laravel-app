9e42d7eeb7771dac3675f609bfaa6d95d2f2fac5
Shahid Karimi <karimi.shahid@gmail.com>
2026-09-16 19:14:58 +0200

    kii

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -48,7 +48,7 @@ import { register } from '@/routes';
                 <div
                     class="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
                 >
-                    <h1 class="mb-1 font-medium">Let's get started Fuckers</h1>
+                    <h1 class="mb-1 font-medium">Let's get started Ungli</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
                         Laravel has an incredibly rich ecosystem. <br />We
                         suggest starting with the following.
